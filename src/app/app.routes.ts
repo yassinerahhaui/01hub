1745248a58e4026135eb62910ffdcf9f2fc48469
yrahhaou <yassinerahhaoui12@gmail.com>
2026-08-12 06:15:06 +0100

    add guards to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { LoginPage } from './pages/login-page/login-page';
+import { authGuard } from './core/guards/auth/auth-guard';
+import { guestGuard } from './core/guards/guest/guest-guard';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'login', component: LoginPage }
+  { path: '', component: HomePage, canActivate: [authGuard] },
+  { path: 'login', component: LoginPage, canActivate: [guestGuard] }
 ];
