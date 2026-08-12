6272625b437ba52a42cc5a054ab636fa364716b4
yrahhaou <yassinerahhaoui12@gmail.com>
2026-08-12 06:13:45 +0100

    create auth-guard

---
+++ src/app/core/guards/auth/auth-guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const platformId = inject(PLATFORM_ID);
+  const router = inject(Router);
+
+  if (isPlatformBrowser(platformId)) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      // Return a UrlTree to redirect seamlessly
+      return router.createUrlTree(['/login']);
+    }
+    return true;
+  }
+  return true;
+};
