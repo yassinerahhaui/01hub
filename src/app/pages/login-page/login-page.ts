27cc347283c40ad7dcbc0cc245b316b2e4264f8c
yrahhaou <yassinerahhaoui12@gmail.com>
2026-08-12 06:10:27 +0100

    create login page

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { form, FormField, FormRoot, required } from '@angular/forms/signals';
 import {LoginModel} from '../../core/models/login-model';
+import { AuthService } from '../../core/services/auth-service/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -9,18 +11,27 @@ import {LoginModel} from '../../core/models/login-model';
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  serverError = signal<string | null>(null);
+
   loginData = signal<LoginModel>({
     email: '',
     password: '',
   });
+
   loginForm = form(this.loginData,(schema) => {
-    required(schema.email, { message: 'Email OR username is required' });
-    required(schema.password, { message: 'Password is required' });
+    required(schema.email, { message: '(Email OR username) is required!' });
+    required(schema.password, { message: 'Password is required!' });
   });
 
   onSubmit() {
     if (this.loginForm().valid()) {
-      console.log('Form submitted:', this.loginData());
+      this.serverError.set(null);
+      this.authService.login(this.loginData()).subscribe({
+        next: () => this.router.navigate(['/']),
+        error: (err) => this.serverError.set('Invalid email or password. Please try again.')
+      });
     } else {
       console.log('Form is invalid');
     }
