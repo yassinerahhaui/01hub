c2a44db6381ec23d684f621049ef56a4956365c2
yrahhaou <yassinerahhaoui12@gmail.com>
2026-08-12 07:58:37 +0100

    add interceptor to config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
